f77b1a7fdd8169b71fee3e7acd3d512decbaa551
harmeetjaipur <harmeet.jaipur@gmail.com>
2018-09-11 18:11:55 -0400

    add game achievements

--- pages/userProfile/index.js
+++ pages/userProfile/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 // styled component(s)
-import styles, { App, UserProfile, GamerScore } from './styles';
+import classes, { App, UserProfile, GamerScore } from './styles';
 
 const stubUser = {
   pic: '//image.ibb.co/jsjxfU/Selection_002.png',
@@ -12,31 +12,71 @@ const stubUser = {
   location: 'Toronto, Canada',
 };
 
-const Profile = ({ user = stubUser }) => (
+const stubachievements = [
+  {
+    id: 1,
+    name: 'Game Name',
+    achievements: ['one', 'two', 'three'],
+  },
+  {
+    id: 2,
+    name: 'Game Name',
+    achievements: ['one', 'two', 'three'],
+  },
+  {
+    id: 3,
+    name: 'Game Name',
+    achievements: ['one', 'two', 'three'],
+  },
+  {
+    id: 4,
+    name: 'Game Name',
+    achievements: ['one', 'two', 'three'],
+  },
+];
+
+const Profile = ({ user }) => (
   <UserProfile>
-    <div className={styles.profileInfo}>
-      <p className={styles.pic}>
+    <div className={classes.profileInfo}>
+      <p className={classes.pic}>
         <img src={user.pic} />
       </p>
       <p>{user.name}</p>
       <p>Last seen: {user.lastLogin}</p>
       <GamerScore>
         <p className="score">
-          Rank: <span> {user.score}</span>
+          Rank <span> {user.score}</span>
         </p>
         <p className="rating">
-          Rating: <span> {user.rating}</span>
+          Rating <span> {user.rating}</span>
         </p>
       </GamerScore>
     </div>
   </UserProfile>
 );
 
+const Leaderboard = ({ achievements }) => (
+  <div className={classes.leaderboard}>
+    <ul className={classes.list}>
+      {achievements.map(({ name, id, achievements }) => (
+        <li key={`game_${id}`}>
+          Game
+          <span className={classes.gameName}>{name}</span>
+          <span className={classes.achievements}>
+            {achievements.join(', ')}
+          </span>
+        </li>
+      ))}
+    </ul>
+  </div>
+);
+
 class SharkShift extends Component {
   render() {
     return (
       <App>
-        <Profile />
+        <Profile user={stubUser} />
+        <Leaderboard achievements={stubachievements} />
       </App>
     );
   }
--- pages/userProfile/styles.js
+++ pages/userProfile/styles.js
@@ -3,7 +3,7 @@ import styled, { css } from 'react-emotion';
 // constants
 const colors = {
   blackBorder: '#333333',
-  blackBg: '#111111',
+  blackBg: '#000',
   whiteBg: '#edebd7',
   whiteText: '#fafafa',
   blackText: '#333333',
@@ -22,29 +22,35 @@ export const containerStyle = {
 };
 
 export const App = styled('div')(() => ({
+  background:
+    'url("https://firebasestorage.googleapis.com/v0/b/norblock-c98d8.appspot.com/o/constantin-popescu-596249-unsplash.jpg?alt=media&token=81050255-fadd-4eb0-8944-33dd5c4bbc9d") right top/100% no-repeat',
   backgroundColor: colors.blackBg,
   color: colors.whiteText,
   display: 'flex',
   flexDirection: 'row',
-  flexBasis: '50%',
   fontSize: '16px',
   fontFamily: '"Lato", sans-serif',
+  '*': {
+    listStyle: 'none',
+  },
 }));
 
 export const UserProfile = styled('div')(() => ({
   padding: '2em',
+  width: '50%',
 }));
 
 export const GamerScore = styled('div')(() => ({
   display: 'flex',
   flexDirection: 'row',
-  flexBasis: '50%',
-  '.score': {
+  '.score, .rating': {
     fontSize: '1em',
+    width: '50%',
     color: colors.whiteText,
-    spang: {
+    span: {
       color: colors.red,
       fontSize: '2em',
+      display: 'block',
     },
   },
 }));
@@ -60,6 +66,7 @@ const pic = css({
   overflow: 'hidden',
   height: 100,
   width: 100,
+  margin: 'auto',
   border: `10px solid ${colors.blackBorder}`,
   img: {
     maxWidth: '100%',
@@ -67,7 +74,24 @@ const pic = css({
   },
 });
 
+const leaderboard = css({
+  width: '50%',
+  padding: '2em',
+  background: 'rgba(255, 255, 255, 0.1)',
+});
+
+const achievements = css({
+  display: 'block',
+});
+
+const gameName = css({
+  display: 'block',
+});
+
 export default {
   profileInfo,
   pic,
+  leaderboard,
+  achievements,
+  gameName,
 };
