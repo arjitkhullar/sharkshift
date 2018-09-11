e27878e09dad0474a18e6df11281fcead8971914
harmeet <harmeet@northernblock.ca>
2018-09-11 16:23:38 -0400

    add user profile component

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,22 @@
-import App, {Container} from 'next/app'
-import React from 'react'
-import withReduxStore from '../lib/with-redux-store'
-import { Provider } from 'react-redux'
+import App, { Container } from 'next/app';
+import React from 'react';
+import withReduxStore from '../lib/with-redux-store';
+import { Provider } from 'react-redux';
+
+// styled component(s)
+import { containerStyle } from '../components/app/styles';
 
 class MyApp extends App {
-  render () {
-    const {Component, pageProps, reduxStore} = this.props
+  render() {
+    const { Component, pageProps, reduxStore } = this.props;
     return (
-      <Container>
+      <Container className={containerStyle}>
         <Provider store={reduxStore}>
           <Component {...pageProps} />
         </Provider>
       </Container>
-    )
+    );
   }
 }
 
-export default withReduxStore(MyApp)
+export default withReduxStore(MyApp);
