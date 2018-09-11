3b3174e2ef2c5f380a09aee41fe46e9223ace2a0
harmeetjaipur <harmeet.jaipur@gmail.com>
2018-09-11 16:50:27 -0400

    add missing styles file

---
+++ components/app/styles.js
@@ -0,0 +1,56 @@
+import styled, { css } from 'react-emotion';
+
+// constants
+const colors = {
+  blackBorder: '#333333',
+  blackBg: '#111111',
+  whiteBg: '#edebd7',
+  whiteText: '#fafafa',
+  blackText: '#333333',
+  red: '#ed6a5a',
+  yellow: '#e3b23c',
+};
+
+// styles
+const profileInfo = css({
+  width: '100%',
+  textAlign: 'center',
+});
+
+const pic = css({
+  borderRadius: '50%',
+  overflow: 'hidden',
+  border: `10px solid ${colors.blackBorder}`,
+  img: {
+    height: 'auto',
+    maxWidth: 100,
+  },
+});
+
+// styled components
+export const containerStyle = {
+  padding: 0,
+  margin: 0,
+  '*': {
+    padding: 0,
+    margin: 0,
+  },
+};
+
+export const App = styled('div')(() => ({
+  backgroundColor: colors.blackBg,
+  color: colors.whiteText,
+  display: 'flex',
+  flexDirection: 'row',
+  flexBasis: '50%',
+  fontSize: '16px',
+}));
+
+export const UserProfile = styled('div')(() => ({
+  padding: '2em',
+}));
+
+export default {
+  profileInfo,
+  pic,
+};
